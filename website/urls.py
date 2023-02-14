c677299ddd47ba5e16955ea1e2b3f493705d52b9
Andrii Tugai <andrii.tugai@gmail.com>
2023-02-14 10:59:57 +0200

    Added login-logout, history features

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('suggest', views.suggest, name='suggest'),
-    path('info', views.info, name='info')
+    path('suggest/', views.suggest, name='suggest'),
+    path('info/', views.info, name='info'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('past', views.past, name='past'),
+    path('delete_past/<past_id>', views.delete_past, name='delete_past'),
 ]
+ 
--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 import os
 import openai
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from .models import Code
 
 lang_list = [
 		'c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 
@@ -41,6 +44,11 @@ def home(request):
 				)
 			# parse the response
 			fixed_code = (response["choices"][0]["text"]).strip()
+
+			# Save fixed code to DataBase
+			record = Code(question=code, code_answer=fixed_code, language=lang, user=request.user)
+			record.save()
+
 			return render(request, 'home.html', {'lang_list':lang_list, 'response': fixed_code, 'lang': lang})
 
 		except Exception as e:
@@ -79,9 +87,71 @@ def suggest(request):
 				)
 			# parse the response
 			fixed_code = (response["choices"][0]["text"]).strip()
+
+			# Save fixed code to DataBase
+			record = Code(question=code, code_answer=fixed_code, language=lang, user=request.user)
+			record.save()
+
 			return render(request, 'suggest.html', {'lang_list':lang_list, 'response': fixed_code, 'lang': lang})
 
 		except Exception as e:
 			return render(request, 'suggest.html', {'lang_list':lang_list, 'code': e, 'lang': lang})
 
 	return render(request, 'suggest.html', {'lang_list':lang_list})
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You have been logged in!")
+		else:
+			messages.success(request, "Error Logging In. Try Again.")
+		return redirect('home')
+	else:
+		return render(request, 'home')
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You have been logged Out...")
+	return redirect('home')
+
+def register_user(request):
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(request, username=username, password=password)
+			login(request, user)
+			messages.success(request, "You have successfuly registered...")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+
+	return render(request, 'register.html', {"form": form})
+
+
+def past(request):
+	if request.user.is_authenticated:
+		code = Code.objects.filter(user_id=request.user.id)
+		return render(request, 'past.html', {'code': code})
+	messages.success(request, "You must be logged in...")
+	return redirect('home')
+
+def delete_past(request, past_id):
+	past = Code.objects.get(pk=past_id)
+	past.delete()
+	messages.success(request, "Deleted Successfully...")
+	return redirect('past')
+
+
+
+
+
+
+
+
